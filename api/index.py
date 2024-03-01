caec3c9fb5dd1944476766cf7772fd7a268d6279
Nazam-Kalsi <nzamkalsi@gmail.com>
2024-03-01 11:35:13 +0530

    initial commit

--- api/index.py
+++ api/index.py
@@ -1,99 +1,106 @@
+# %%
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
 
 
-from flask import Flask, request, jsonify
 import spacy
 import pandas as pd
-import nltk
-from nltk import word_tokenize, pos_tag, ne_chunk
-
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
-from flask_cors import CORS
 
-# app = Flask(__name__)
-# CORS(app)
+# %%
+nlp = spacy.load("en_core_web_sm")
 
-# Load Spacy model
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('punkt')
+# %%
+existing_data = pd.read_csv(r'api/ipc_sections.csv')
+
+# %%
+def extract_entities(text):
+    doc = nlp(text)
+    return [ent.text.lower() for ent in doc.ents]
+
+# %%
+existing_data['Entities'] = existing_data['Offense'].apply(extract_entities).dropna()
+
+
+# %%
+flattened_data = existing_data[['Section', 'Punishment', 'Entities']].explode('Entities')
+
+
+# %%
+label_encoder = LabelEncoder()
+flattened_data['Section_Label'] = label_encoder.fit_transform(flattened_data['Section'])
+
+
+# %%
+def extract_entities(text):
+    doc = nlp(text)
+    return ' '.join([ent.text.lower() for ent in doc.ents])
+
+# %%
+existing_data['Entities'] = existing_data['Offense'].apply(extract_entities)
+
+# %%
+existing_data.count()
+
+
+# %%
+existing_data['Entities'].sample(10)
+
+# %%
+flattened_data['Entities'] = flattened_data['Entities'].fillna('')
+tfidf_vectorizer = TfidfVectorizer(lowercase=True)
+tfidf_matrix = tfidf_vectorizer.fit_transform(flattened_data['Entities'])
+
+
+# %%
+classifier = SVC(kernel='linear')
+classifier.fit(tfidf_matrix, flattened_data['Section_Label'])
+
+# %%
+def predict_section_and_punishment(user_input):
+    user_entities = extract_entities(user_input)
+    
+    if isinstance(user_entities, str):
+        user_entities = [user_entities]
+    
+    user_tfidf = tfidf_vectorizer.transform(user_entities)
+    predicted_label = classifier.predict(user_tfidf)
+    
+    predicted_section = label_encoder.inverse_transform(predicted_label)
+    
+    punishment = existing_data[existing_data['Section'] == predicted_section[0]]['Punishment'].iloc[0]
+    
+    # return predicted_section[0], punishment
+    return predicted_section[0]
+    return {'User Input':'predicted_section[0]'}
 
 class handler(BaseHTTPRequestHandler):
-        def do_GET(self):
-             self.send_response(200)
-             self.send_header('Content-type', 'text/plain')
-             self.end_headers()
-            #  self.predict_section_and_punishment(user_input)
-# Load existing data
-        existing_data = pd.read_csv('api/ipc_sections.csv')
+    def do_GET(self):
+        self.send_response(200)
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        if "userInput" in dic:
+            userInput =dic[userInput]
+        self.wfile.write(predict_section_and_punishment(userInput))
+        return
 
-# Function to extract entities
-        def extract_entities_nltk(text):
-            words = word_tokenize(text)
-            tagged = pos_tag(words)
-            entities = ne_chunk(tagged)
-            return ' '.join([entity[0] for entity in entities if isinstance(entity, tuple)])
-
-            # Apply entity extraction to the existing data
-            existing_data['Entities'] = existing_data['Offense'].apply(extract_entities_nltk)
-
-            # Flatten the data
-            flattened_data = existing_data[['Section', 'Punishment', 'Entities']].explode('Entities')
-
-            # Label encode the sections
-            label_encoder = LabelEncoder()
-            flattened_data['Section_Label'] = label_encoder.fit_transform(flattened_data['Section'])
+# %%
+# user_input = '''I am writing to report an attempted murder incident that occurred on December 10, 2023, at 11:00 PM, in [Location]. The victim, Mr. Ranveer Kumar, narrowly escaped harm during this event. I urgently seek your attention to this matter for a swift investigation.'''
+# section, punishment = predict_section_and_punishment(user_input)
 
 
-
-            tfidf_vectorizer = TfidfVectorizer(lowercase=True)
-            tfidf_matrix = tfidf_vectorizer.fit_transform(flattened_data['Entities'])
-
-            classifier = SVC(kernel='linear')
-            classifier.fit(tfidf_matrix, flattened_data['Section_Label'])
-
-            def predict_section_and_punishment(user_input):
-                user_entities = extract_entities_nltk(user_input)
-
-                if isinstance(user_entities, str):
-                    user_entities = [user_entities]
-
-                user_tfidf = tfidf_vectorizer.transform(user_entities)
-                predicted_label = classifier.predict(user_tfidf)
-
-                predicted_section = label_encoder.inverse_transform(predicted_label)
-                punishment = existing_data[existing_data['Section'] == predicted_section[0]]['Punishment'].iloc[0]
-
-                return predicted_section[0], punishment
-            #     data= predicted_section[0], punishment
-            #     data = json.dumps(data, ensure_ascii = False)
-		    #     json_value = json.dumps(data, ensure_ascii = False)
-		    #    result = json.loads(json_value)
-		    #     self.wfile.write(result.encode('utf8'))
-		    #     return            
-               
-
-# @app.route('/predict_section_and_punishment', methods=['POST'])
-# def predict_endpoint():
-#     try:
-#         user_input = request.json['userInput']
-#         section, punishment = predict_section_and_punishment(user_input)
-#         return jsonify({'result1': section, 'result2': punishment})
-#     except Exception as e:
-#         return jsonify({'error': str(e)})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+# # %%
+# if section and punishment:
+#     print(f"User Input: {user_input}")
+#     print(f"Predicted Section: {section}")
+#     print(f"Predicted Punishment: {punishment}")
+# else:
+#     print("No match found for the input.")
 
 
-    # class handler(BaseHTTPRequestHandler):
-    #     def do_GET(self):
-    #          self.send_response(200)
-    #          self.send_header('Content-type', 'text/plain')
-    #          self.end_headers()
-    #          self.predict_section_and_punishment(user_input)
